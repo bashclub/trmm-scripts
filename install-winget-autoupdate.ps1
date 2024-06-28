a8cce43f48a122121ea574f0ae329b6130dd2bb4
Thorsten Spille <thorstenspille@users.noreply.github.com>
2024-06-28 12:04:06 +0200

    Update install-winget-autoupdate.ps1

--- install-winget-autoupdate.ps1
+++ install-winget-autoupdate.ps1
@@ -1,15 +1,10 @@
-#Set custom field at client level with LIZENSE an GROUP ID
-###Download and Install Securepount AV  Client###
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$downloadURL = "https://github.com/Romanitho/Winget-AutoUpdate/releases/download/v1.20.0/WAU.zip"
+$downloadURL = "https://github.com/Romanitho/Winget-AutoUpdate/releases/download/v1.20.1/WAU.zip"
 
 
-#---------------------------------------------------------------#
-
-#Look for Securepoint Folder, if not exist then create
-$folderName = "Spille"
+$folderName = "IMSTALL"
 $Path="C:\"+$folderName
 
 if (!(Test-Path $Path))
@@ -21,7 +16,6 @@ else
 write-host "Folder already exists"
 }
 
-#Download MSI for SECUREPOINT AV
-Invoke-WebRequest -Uri "$downloadURL" -OutFile "C:\Spille\WAU.zip"
-Expand-Archive -Path C:\Spille\WAU.zip -DestinationPath C:\Spille -Force
-C:\Spille\Winget-AutoUpdate-Install.ps1 -Silent
+Invoke-WebRequest -Uri "$downloadURL" -OutFile "C:\IMSTALL\WAU.zip"
+Expand-Archive -Path C:\IMSTALL\WAU.zip -DestinationPath C:\IMSTALL -Force
+C:\IMSTALL\Winget-AutoUpdate-Install.ps1 -Silent
